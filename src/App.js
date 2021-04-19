803c8a002d60b46e05a5d566c75f57e3ef3ad7c5
Anand Borkar <borkaranand235@gmail.com>
2021-04-20 00:34:12 +0530

    Modify Styling and Layout

--- src/App.js
+++ src/App.js
@@ -1,19 +1,12 @@
+import Home from './Components/Home'
+import Compare from './Components/Compare'
+import About from './Components/About'
+
 import './App.css'
-// import Welcome from './Components/Welcome'
-import Cardone from './Components/Cardone'
 
 function App() {
-	const cardone = { "margin": "10vh 0vw 0vh 5vw" }
-	const cardtwo = { "margin": "10vh 0vw 0vh 40vw" }
-	const cardthree = { "margin": "55vh 0vw 0vh 5vw", "width": "65vw", "height": "40vh" }
-	const cardfour = { "margin": "10vh 0vw 0vh 73vw", "width": "25vw", "height": "60vh" }
 	return (
-		<div>
-			<Cardone prop = { cardone } />
-			<Cardone prop = { cardtwo } />
-			<Cardone prop = { cardthree } />
-			<Cardone prop = { cardfour } />
-		</div>
+		<Home />
 	)
 }
 
