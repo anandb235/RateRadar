7053a32f157902aae125167bffd3607a9e58cc65
Anand Borkar <borkaranand235@gmail.com>
2021-04-20 01:13:21 +0530

    Add Compare Page

---
+++ src/Components/Compare.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Card from './MiniComponents/Card'
+
+function Compare() {
+
+    const compareChart = { "margin": "30vh 0vw 0vh 20vh", "width": "80vw", "height": "65vh" }
+
+    return (
+        <Card prop = { compareChart } />
+    )
+}
+
+export default Compare
