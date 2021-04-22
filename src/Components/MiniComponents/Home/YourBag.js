fa61a8c4d46a48a6fe20d291a1a060f605bf0131
Anand Borkar <borkaranand235@gmail.com>
2021-04-22 22:34:57 +0530

    Add YourBag - Home

---
+++ src/Components/MiniComponents/Home/YourBag.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function YourBag() {
+
+    const yourbag = { "margin": "12vh 10vw 0vh 70vw", "width": "25vw", "height": "45vh" }
+
+    return (
+        <card className="card" style={ yourbag }>
+            <div className="bag-head">
+                <span className="bag-title">Your Bag</span>
+            </div>
+            <div className="vals">
+                <span className="current-val">000<span className="current-val-dec">.000</span><span className="currency">INR</span></span>
+                <span className="gain-loss">Gain/Loss: <span className="gain-loss-val">-1.69</span> </span>
+                <span className="invested">Invested: <span className="invested-val">200.00</span> </span>
+            </div>
+        </card>
+    )
+}
+
+export default YourBag
