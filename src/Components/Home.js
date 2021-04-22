62a98cd4bc916f44f2873d240eedf93cbbce86c3
Anand Borkar <borkaranand235@gmail.com>
2021-04-22 22:17:34 +0530

    Add Crypts - Home

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,5 +1,21 @@
 import React from 'react'
-import Card from './MiniComponents/Card'
+import '../style/Card.css'
+import '../style/Home.css'
+
+// import "../data/crypts.json"
+
+// const data = {
+//     "one": {
+//         "name": "Bitcoin",
+//         "price": 43278273.323,
+//         "growth": -2.33
+//     },
+//     "two": {
+//         "name": "Bitcoin",
+//         "price": 43278273.323,
+//         "growth": -2.33
+//     }
+// }
 
 function Home() {
 
@@ -9,12 +25,89 @@ function Home() {
 	const pincharts_2 = { "margin": "62vh 0vw 0vh 55vw", "width": "40vw", "height": "35vh" }
     
     return (
-        <div>
-			<Card prop = { currency } />
-			<Card prop = { yourbag } />
-			<Card prop = { pincharts_1 } />
-			<Card prop = { pincharts_2 } />
-		</div>
+        <card>
+			<card className="card" style={ currency }>
+				<card-head>
+					<card-title>Crypts</card-title>
+					<currency-converter style={ { "text-align": "right" } }>
+						Currency: <current-currency>INR</current-currency>
+					</currency-converter>
+				</card-head>
+				<table>
+					<tbody>
+						<tr>
+							<th>Currency</th>
+							<th>Price</th>
+							<th>Growth</th>
+						</tr>
+					</tbody>
+				</table>
+
+				<table className="data-table">
+
+					{/* {data.map(data => (
+						<tr>
+							<td>{data.name}</td>
+							<td>{data.price}</td>
+							<td>{data.growth}</td>
+						</tr>
+					))} */}
+
+					<tbody>
+						<tr>
+							<td>BitCoin</td>
+							<td>4423923.233</td>
+							<td>-2.33</td>
+						</tr>
+
+						<tr>
+							<td>BitCoin</td>
+							<td>4423923.233</td>
+							<td>-2.33</td>
+						</tr>
+
+						<tr>
+							<td>BitCoin</td>
+							<td>4423923.233</td>
+							<td>-2.33</td>
+						</tr>
+
+						<tr>
+							<td>BitCoin</td>
+							<td>4423923.233</td>
+							<td>-2.33</td>
+						</tr>
+
+						<tr>
+							<td>BitCoin</td>
+							<td>4423923.233</td>
+							<td>-2.33</td>
+						</tr>
+
+						<tr>
+							<td>BitCoin</td>
+							<td>4423923.233</td>
+							<td>-2.33</td>
+						</tr>
+
+						<tr>
+							<td>BitCoin</td>
+							<td>4423923.233</td>
+							<td>-2.33</td>
+						</tr>
+
+						<tr>
+							<td>BitCoin</td>
+							<td>4423923.233</td>
+							<td>-2.33</td>
+						</tr>
+					</tbody>
+				</table>
+			</card>
+			<card className="card" style={ yourbag }></card>
+			<card className="card" style={ pincharts_1 }></card>
+			<card className="card" style={ pincharts_2 }></card>
+		</card>
     )
 }
 
