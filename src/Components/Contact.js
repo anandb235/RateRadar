0e1b19c2593e32b9cc480ccfdbef15fb649bf7c1
Anand Borkar <borkaranand235@gmail.com>
2021-04-23 20:32:40 +0530

    Add Contact Page

---
+++ src/Components/Contact.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import '../style/Card.css'
+import '../style/Contact.css'
+
+function Contact() {
+    const contactCss = { "margin": "22vh 0vw 0vh 33vw", "width": "34vw", "height": "63vh" }
+
+    return (
+        <div className="card contact-card" style={ contactCss }>
+            
+        </div>
+    )
+}
+
+export default Contact
