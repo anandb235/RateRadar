2c2c9ba72a2ab452f7fb1abd2c6a0b8e55602eac
Anand Borkar <borkaranand235@gmail.com>
2021-04-30 16:27:34 +0530

    Remove Warnings
    Signed-off-by: Anand Borkar <borkaranand235@gmail.com>

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -13,9 +13,9 @@ function Navbar() {
         <div style={ navStyle }>
             <ham-burger style={{ "height": "3vh", "width": "4vw" }}>
                 <svg viewBox="0 0 120 120">
-                    <line x1="10" y1="0" x2="100" y2="0" style={{ "stroke": "#3b4ab8", "stroke-width": "11" }} />
-                    <line x1="10" y1="25" x2="100" y2="25" style={{ "stroke": "#3b4ab8", "stroke-width": "5.5" }} />
-                    <line x1="10" y1="50" x2="100" y2="50" style={{ "stroke": "#3b4ab8", "stroke-width": "6.5" }} />
+                    <line x1="10" y1="0" x2="100" y2="0" style={{ "stroke": "#3b4ab8", "strokeWidth": "11" }} />
+                    <line x1="10" y1="25" x2="100" y2="25" style={{ "stroke": "#3b4ab8", "strokeWidth": "5.5" }} />
+                    <line x1="10" y1="50" x2="100" y2="50" style={{ "stroke": "#3b4ab8", "strokeWidth": "6.5" }} />
                 </svg>
             </ham-burger>
             <rate-radar style={ { "color": "#fff", "fontSize": "40px", "marginTop": "-2vh", "marginLeft": "1vw" } }>
