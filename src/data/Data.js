3c39f1e3363cc8d06d0e2e104a9c1ae9acbee890
Anand Borkar <borkaranand235@gmail.com>
2021-04-30 15:27:02 +0530

    Add Data ID

--- src/data/Data.js
+++ src/data/Data.js
@@ -1,119 +1,82 @@
 const data = [
     {
+        "id": 1,
         "name": "Bitcoin",
         "price": 43278273.323,
         "growth": -2.33
     },
     {
+        "id": 2,
         "name": "Bitcoin",
         "price": 43278273.323,
         "growth": -2.33
     },
     {
+        "id": 3,
         "name": "Bitcoin",
         "price": 43278273.323,
         "growth": -2.33
     },
     {
+        "id": 4,
         "name": "Bitcoin",
         "price": 43278273.323,
         "growth": -2.33
     },
     {
+        "id": 5,
         "name": "Bitcoin",
         "price": 43278273.323,
         "growth": -2.33
     },
     {
+        "id": 6,
         "name": "Bitcoin",
         "price": 43278273.323,
         "growth": -2.33
     },
     {
+        "id": 7,
         "name": "Bitcoin",
         "price": 43278273.323,
         "growth": -2.33
     },
     {
+        "id": 8,
         "name": "Bitcoin",
         "price": 43278273.323,
         "growth": -2.33
     },
     {
+        "id": 10,
         "name": "Bitcoin",
         "price": 43278273.323,
         "growth": -2.33
     },
     {
+        "id": 11,
         "name": "Bitcoin",
         "price": 43278273.323,
         "growth": -2.33
     },
     {
+        "id": 12,
         "name": "Bitcoin",
         "price": 43278273.323,
         "growth": -2.33
     },
     {
+        "id": 13,
         "name": "Bitcoin",
         "price": 43278273.323,
         "growth": -2.33
     },
     {
+        "id": 14,
         "name": "Bitcoin",
         "price": 43278273.323,
         "growth": -2.33
     },
-    {
-        "name": "Bitcoin",
-        "price": 43278273.323,
-        "growth": -2.33
-    },
-    {
-        "name": "Bitcoin",
-        "price": 43278273.323,
-        "growth": -2.33
-    },
-    {
-        "name": "Bitcoin",
-        "price": 43278273.323,
-        "growth": -2.33
-    },
-    {
-        "name": "Bitcoin",
-        "price": 43278273.323,
-        "growth": -2.33
-    },
-    {
-        "name": "Bitcoin",
-        "price": 43278273.323,
-        "growth": -2.33
-    },
-    {
-        "name": "Bitcoin",
-        "price": 43278273.323,
-        "growth": -2.33
-    },
-    {
-        "name": "Bitcoin",
-        "price": 43278273.323,
-        "growth": -2.33
-    },
-    {
-        "name": "Bitcoin",
-        "price": 43278273.323,
-        "growth": -2.33
-    },
-    {
-        "name": "Bitcoin",
-        "price": 43278273.323,
-        "growth": -2.33
-    },
-    {
-        "name": "Bitcoin",
-        "price": 43278273.323,
-        "growth": -2.33
-    }
 ]
 
 export default data
