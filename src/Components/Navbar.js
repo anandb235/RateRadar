7dc5708eccf683186ccf03c655c02ca641efdc71
Anand Borkar <borkaranand235@gmail.com>
2021-05-10 22:12:27 +0530

    Route using react-router-dom

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 
 import '../style/Navbar.css'
 import Logo from '../assets/RateRadar.svg'
 
 function Navbar() {
+
+    const navStyle = { "textDecoration": "none", "color": "#ffffff" }
 
     return (
         <div className="navStyle">
@@ -13,10 +16,26 @@ function Navbar() {
             <div className="nav">
                 <span>Rate<span style={ { "color": "#3b4ab8" } }>Radar</span>&nbsp;&nbsp;|&nbsp;&nbsp;</span>
                 <div className="navs">
-                    <span className="active">Home</span>
-                    <span>Compare</span>
-                    <span>About</span>
-                    <span>Contact</span>
+                    <span className="nav-item">
+                        <NavLink style={ navStyle } exact to="/">
+                            <span id="home">Home</span>
+                        </NavLink>
+                    </span>
+                    <span className="nav-item">
+                        <NavLink style={ navStyle } to="/compare">
+                            <span id="compare">Compare</span>
+                        </NavLink>
+                    </span>
+                    <span className="nav-item">
+                        <NavLink style={ navStyle } to="/about">
+                            <span id="about">About</span>
+                        </NavLink>
+                    </span>
+                    <span className="nav-item">
+                        <NavLink style={ navStyle } to="/contact">
+                            <span id="contact">contact</span>
+                        </NavLink>
+                    </span>
                 </div>
                 <div className="bag-btn">
                     <button>Populate Bag</button>
