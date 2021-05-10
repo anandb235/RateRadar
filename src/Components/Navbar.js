14ef7dc54508bd777cc17ea31305d221a2edf757
Anand Borkar <borkaranand235@gmail.com>
2021-05-10 21:46:12 +0530

    Add Bag Button - Home

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -18,6 +18,9 @@ function Navbar() {
                     <span>About</span>
                     <span>Contact</span>
                 </div>
+                <div className="bag-btn">
+                    <button>Populate Bag</button>
+                </div>
             </div>
         </div>
     )
