24e5d92c1382d913fb441adf8981c02bbeae1bf6
Anand Borkar <borkaranand235@gmail.com>
2021-05-10 22:12:27 +0530

    Route using react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+
 import Home from './Components/Home'
-// import Compare from './Components/Compare'
-// import About from './Components/About'
-// import Contact from './Components/Contact'
+import Compare from './Components/Compare'
+import About from './Components/About'
+import Contact from './Components/Contact'
 
 import Navbar from './Components/Navbar'
 
@@ -9,10 +11,15 @@ import './App.css'
 
 function App() {
 	return (
-		<div>
+		<Router>
 			<Navbar />
-			<Home />
-		</div>
+			<Switch>
+				<Route path="/" exact component={ Home } />
+				<Route path="/compare" component={ Compare } />
+				<Route path="/about" component={ About } />
+				<Route path="/contact" component={ Contact } />
+			</Switch>
+		</Router>
 	)
 }
 
