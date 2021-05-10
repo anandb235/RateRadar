84b356563528dffff9231e6a2bf5ddfe4f9f20e7
Anand Borkar <borkaranand235@gmail.com>
2021-05-10 20:23:32 +0530

    Add Logo - Home

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,27 +1,18 @@
 import React from 'react'
 
+import '../style/Navbar.css'
+import Logo from '../assets/RateRadar.svg'
+
 function Navbar() {
 
-    const navStyle = {
-        "position": "absolute",
-        "margin": "3vh 0vw 0vh 2vw",
-        "display": "flex",
-        "justifyContent": "center"
-    }
-
     return (
-        <div style={ navStyle }>
-            <ham-burger style={{ "height": "3vh", "width": "4vw" }}>
-                <svg viewBox="0 0 120 120">
-                    <line x1="10" y1="0" x2="100" y2="0" style={{ "stroke": "#3b4ab8", "strokeWidth": "11" }} />
-                    <line x1="10" y1="25" x2="100" y2="25" style={{ "stroke": "#3b4ab8", "strokeWidth": "5.5" }} />
-                    <line x1="10" y1="50" x2="100" y2="50" style={{ "stroke": "#3b4ab8", "strokeWidth": "6.5" }} />
-                </svg>
-            </ham-burger>
-            <rate-radar style={ { "color": "#fff", "fontSize": "40px", "marginTop": "-2vh", "marginLeft": "1vw" } }>
-                <span>Rate</span>
-                <span style={ { "color": "#3b4ab8" } }>Radar</span>
-            </rate-radar>
+        <div className="navStyle">
+            <div className="logo">
+                <img src={Logo} alt="RR" />
+            </div>
+            <div className="nav">
+                <span>Rate<span style={ { "color": "#3b4ab8" } }>Radar</span>&nbsp;&nbsp;|&nbsp;&nbsp;</span>
+            </div>
         </div>
     )
 }
