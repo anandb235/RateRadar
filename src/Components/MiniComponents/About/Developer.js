99791a6933d5492d8f79fd931efcad45f74f74bc
Anand Borkar <borkaranand235@gmail.com>
2021-05-16 16:17:32 +0530

    Add Developer Component - About

---
+++ src/Components/MiniComponents/About/Developer.js
@@ -0,0 +1,38 @@
+import React from 'react'
+
+import SelfImage from '../../../assets/SelfImage.jpg'
+
+function Developer() {
+
+    const selfImage = { "margin": "65vh 0vw 0vh 18vw", "width": "200px", "height": "200px" }
+    const dev = { "margin": "65vh 0vw 0vh 37vw", "width": "40vw", "height": "30vh" }
+
+	return (
+		<div>
+			<div className="role">Developer</div>
+			<img src={ SelfImage } style={ { ...selfImage, "borderRadius": "50%" } } alt="Logo"></img>
+			<div className="card about" style={ dev }>
+                <p>Myself,&nbsp;
+                    <span style={{ "color": "#3b4ab8", "fontWeight": "bolder" }}>
+                        Samprit Chaurasiya
+                    </span>,<br />
+                    a Web Development aficionado studying Bachelor of Engineering<br /> @
+                    <a
+                        href="http://siesgst.edu.in"
+                        style={{ "color": "#3b4ab8" }}>
+                        SIESGST, Navi Mumbai
+                    </a><br />
+                    <span style={{ "fontSize": "20px" }}>Contribute to this project&nbsp;
+                        <a
+                            href="https://github.com/anandb235/RateRadar"
+                            style={{ "color": "#3b4ab8", "textDecoration": "none" }}>
+                            here
+                        </a>
+                    </span>
+                </p>
+            </div>
+		</div>
+	)
+}
+
+export default Developer
