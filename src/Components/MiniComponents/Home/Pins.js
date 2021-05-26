109757ae9f9b79d37aeb3250c03dd81f6ce195d6
Anand Borkar <borkaranand235@gmail.com>
2021-05-26 19:25:01 +0530

    Merge DropdownPin in Pins - Home

--- src/Components/MiniComponents/Home/Pins.js
+++ src/Components/MiniComponents/Home/Pins.js
@@ -2,11 +2,19 @@ import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 
 import '../../../style/Pins.css'
-import DropdownPin from './DropdownPin'
+import PinCharts1 from './PinCharts1'
+import PinCharts2 from './PinCharts2'
+import ArrowSvg from './ArrowSvg'
+import { Dropdown } from 'semantic-ui-react'
+import 'semantic-ui-css/components/dropdown.css'
+import 'semantic-ui-css/components/reset.css'
+import 'semantic-ui-css/components/transition.css'
 
 function Pins() {
 
 	const [opts, setOpts] = useState([])
+	const [selectedCoin1, setselectedCoin1] = useState('bitcoin')
+	const [selectedCoin2, setselectedCoin2] = useState('ethereum')
 
 	const dropdownData = () => {
 
@@ -16,7 +24,7 @@ function Pins() {
 			.get('https://api.coingecko.com/api/v3/coins')
 			.then(res => {
 				res.data.map(coin => {
-					listItems.push({key: coin.id, text: coin.id, value: coin.id})
+					listItems.push({ key: coin.id, text: coin.id, value: coin.id })
 					return null
 				})
 				setOpts(listItems)
@@ -30,13 +38,45 @@ function Pins() {
 		dropdownData()
 	}, [])
 
+	const handleDropDownSelect1 = (event, data) => {
+		setselectedCoin1(data.value)
+	}
+
+	const handleDropDownSelect2 = (event, data) => {
+		setselectedCoin2(data.value)
+	}
+
 	return (
-		<div className="pins" style={{ position: "absolute" }}>
-			<span className="pin-head" style={{marginBottom: '10px'}} >Pins</span>
-			<DropdownPin default="bitcoin" opts={ opts } />
-			<DropdownPin default="ethereum" opts={ opts } />
-			<button className="apply-btn" style={{marginTop: '10px'}} >Show Charts</button>
-		</div>
+		<>
+			<PinCharts1 coin={selectedCoin1} />
+			<div className="pins" style={{ position: "absolute" }}>
+				<span className="pin-head" style={{ marginBottom: '10px' }} >Pins</span>
+				<div style={{ zIndex: 4 }}>
+					<Dropdown
+						defaultValue="bitcoin"
+						placeholder="Coin 1"
+						onChange={handleDropDownSelect1}
+						search
+						selection
+						style={{ innerWidth: '2em' }}
+						options={opts}
+					/>
+				</div>
+				<ArrowSvg />
+				<div style={{ zIndex: 4 }}>
+					<Dropdown
+						defaultValue="ethereum"
+						placeholder="Coin 2"
+						onChange={handleDropDownSelect2}
+						search
+						selection
+						style={{ innerWidth: '2em' }}
+						options={opts}
+					/>
+				</div>
+			</div>
+			<PinCharts2 coin={selectedCoin2} />
+		</>
 	)
 }
 
