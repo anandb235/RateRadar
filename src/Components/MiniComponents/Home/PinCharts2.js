556d41c281583ab683e2b1833b16468ca25fb504
Anand Borkar <borkaranand235@gmail.com>
2021-05-26 20:01:19 +0530

    Add ArrowSvg - Home

--- src/Components/MiniComponents/Home/PinCharts2.js
+++ src/Components/MiniComponents/Home/PinCharts2.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 import { Line } from 'react-chartjs-2'
 
-function PinCharts2() {
+function PinCharts2(props) {
 
     const pincharts_2 = { "margin": "62vh 0vw 0vh 55vw", "width": "40vw", "height": "35vh" }
 
@@ -13,52 +13,51 @@ function PinCharts2() {
 
     const [chartData, setChartData] = useState({})
 
-    const chart = () => {
-
-        let time = []
-        let price = []
-
-        axios
-            .get("https://api.coingecko.com/api/v3/coins/ethereum/market_chart?vs_currency=usd&days=1&interval=hourly")
-            .then(res => {
-                res.data.prices.map(item => {
-                    time.push(formatter(item[0]))
-                    price.push(item[1]/1000)
-                    return null
-                })
-
-                function formatter(timestamp) {
-                    var date = new Date(timestamp).getDate()
-                    var month = new Date(timestamp).getMonth() + 1
-                    var hour = new Date(timestamp).getHours()
-                    return date + "/" + month + ": " + hour;
-                }
-
-                setChartData({
-                    labels: time,
-                    datasets: [{
-                        label: 'Ethereum',
-                        data: price,
-                        backgroundColor: '#3b4ab8',
-                        borderWidth: 1,
-                        borderColor: '#3b4ab8'
-                    }]
-                })
-            })
-            .catch(err => {
-                console.log(err)
-            })
-    }
-
     useEffect(() => {
-        chart()
-    }, [])
+        const chart = (coin) => {
+
+            let time = []
+            let price = []
+
+            axios
+                .get(`https://api.coingecko.com/api/v3/coins/${coin}/market_chart?vs_currency=usd&days=1&interval=hourly`)
+                .then(res => {
+                    res.data.prices.map(item => {
+                        time.push(formatter(item[0]))
+                        price.push(item[1])
+                        return null
+                    })
+
+                    function formatter(timestamp) {
+                        var date = new Date(timestamp).getDate()
+                        var month = new Date(timestamp).getMonth() + 1
+                        var hour = new Date(timestamp).getHours()
+                        return date + "/" + month + ": " + hour;
+                    }
+
+                    setChartData({
+                        labels: time,
+                        datasets: [{
+                            label: props.coin.toUpperCase(),
+                            data: price,
+                            backgroundColor: '#3b4ab8',
+                            borderWidth: 1,
+                            borderColor: '#3b4ab8'
+                        }]
+                    })
+                })
+                .catch(err => {
+                    console.log(err)
+                })
+        }
+        chart(props.coin)
+    }, [props.coin])
 
     return (
-        <div className="card" style={ pincharts_2 }>
-            <div style={ lineChartStyle }>
+        <div className="card" style={pincharts_2}>
+            <div style={lineChartStyle}>
                 <Line
-                    data={ chartData } />
+                    data={chartData} />
             </div>
         </div>
     )
