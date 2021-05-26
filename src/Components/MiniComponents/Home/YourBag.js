b08b407db10d797576e7bc620747642efc0619f6
Anand Borkar <borkaranand235@gmail.com>
2021-05-26 20:34:12 +0530

    Added tag for coming soon

--- src/Components/MiniComponents/Home/YourBag.js
+++ src/Components/MiniComponents/Home/YourBag.js
@@ -9,6 +9,7 @@ function YourBag() {
             <div className="bag-head">
                 <span className="bag-title">Your Bag</span>
             </div>
+            <div className='tag'>Feature Coming Soon!</div>
             <div className="vals">
                 <span className="current-val">000<span className="current-val-dec">.000</span><span className="currency">INR</span></span>
                 <span className="gain-loss">Gain/Loss: <span className="gain-loss-val">-1.69</span> </span>
