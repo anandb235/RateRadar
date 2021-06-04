f99e26a41ea21237ea651ea582a89ce9e5457085
Anand Borkar <borkaranand235@gmail.com>
2021-06-04 21:27:43 +0530

    Fix error when using stroke-width in Arrow svg

--- src/Components/MiniComponents/Home/ArrowSvg.js
+++ src/Components/MiniComponents/Home/ArrowSvg.js
@@ -4,10 +4,10 @@ function ArrowSvg() {
 	return (
 		<svg width="100%" height="100%" viewBox="0 0 720 150" xmlns="http://www.w3.org/2000/svg">
 			<g>
-				<line stroke-width="15" y2="111" x2="653" y1="111" x1="81" stroke="#3b4ab8" fill="#3b4ab8" />
-				<line stroke-width="15" y2="76" x2="653" y1="76" x1="81" stroke="#3b4ab8" fill="#3b4ab8" />
-				<line stroke-width="15" y2="111" x2="653" y1="148" x1="546" stroke="#3b4ab8" fill="#3b4ab8" />
-				<line stroke-width="15" y2="38.5" x2="187" y1="76" x1="81" stroke="#3b4ab8" fill="#3b4ab8" />
+				<line strokeWidth="15" y2="111" x2="653" y1="111" x1="81" stroke="#3b4ab8" fill="#3b4ab8" />
+				<line strokeWidth="15" y2="76" x2="653" y1="76" x1="81" stroke="#3b4ab8" fill="#3b4ab8" />
+				<line strokeWidth="15" y2="111" x2="653" y1="148" x1="546" stroke="#3b4ab8" fill="#3b4ab8" />
+				<line strokeWidth="15" y2="38.5" x2="187" y1="76" x1="81" stroke="#3b4ab8" fill="#3b4ab8" />
 			</g>
 		</svg>
 	)
