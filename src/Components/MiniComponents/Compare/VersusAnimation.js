c6b97d842c6a806dc4c27baa3355a6cb14c7f7cf
Anand Borkar <borkaranand235@gmail.com>
2021-06-26 17:56:37 +0530

    Add virtualized dropdown css

--- src/Components/MiniComponents/Compare/VersusAnimation.js
+++ src/Components/MiniComponents/Compare/VersusAnimation.js
@@ -1,17 +1,23 @@
 import React from 'react'
 
 function VersusAnimation() {
-	return (
-		<div class="animate">
-			<svg width="120" height="500" viewBox="0 0 200 720">
-				<g id="svg_10">
-					<line stroke-width="4" stroke-linecap="undefined" stroke-linejoin="undefined" id="svg_5" y2="598.5" x2="155.5" y1="381.5" x1="96.5" stroke="#3b4ab8" fill="none" />
-					<text font-style="normal" font-weight="bold" transform="matrix(1.58768, 0, 0, 1.42246, -43.6787, -152.796)" text-anchor="start" font-family="Noto Sans JP" font-size="24" stroke-width="0" id="svg_6" y="368.88533" x="68.38658" stroke="#000" fill="#3b4ab8">V/S</text>
-					<line stroke-width="4" stroke-linecap="undefined" stroke-linejoin="undefined" id="svg_9" y2="338.5" x2="105.5" y1="121.5" x1="44.5" stroke="#3b4ab8" fill="none" />
-				</g>
-			</svg>
-		</div>
-	)
+    return (
+        <div className="animate">
+            <svg width="120" height="500" viewBox="0 0 200 720">
+                <g id="svg_10">
+                    <line strokeWidth="4" strokeLinecap="undefined" strokeLinejoin="undefined" id="svg_5" y2="598.5"
+                          x2="155.5" y1="381.5" x1="96.5" stroke="#3b4ab8" fill="none"/>
+                    <text fontStyle="normal" fontWeight="bold"
+                          transform="matrix(1.58768, 0, 0, 1.42246, -43.6787, -152.796)" textAnchor="start"
+                          fontFamily="Noto Sans JP" fontSize="24" strokeWidth="0" id="svg_6" y="368.88533" x="68.38658"
+                          stroke="#000" fill="#3b4ab8">V/S
+                    </text>
+                    <line strokeWidth="4" strokeLinecap="undefined" strokeLinejoin="undefined" id="svg_9" y2="338.5"
+                          x2="105.5" y1="121.5" x1="44.5" stroke="#3b4ab8" fill="none"/>
+                </g>
+            </svg>
+        </div>
+    )
 }
 
 export default VersusAnimation
