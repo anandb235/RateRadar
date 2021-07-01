8965732e4758010f741d3b7264c182f0323418da
Anand Borkar <borkaranand235@gmail.com>
2021-07-02 00:13:37 +0530

    Use new virtual dropdown in pins

--- src/Components/MiniComponents/Home/Pins.js
+++ src/Components/MiniComponents/Home/Pins.js
@@ -5,14 +5,14 @@ import ArrowSvg from './ArrowSvg'
 import 'semantic-ui-css/components/dropdown.css'
 import 'semantic-ui-css/components/reset.css'
 import 'semantic-ui-css/components/transition.css'
-import VirtualizedDropdown from "./VirtualizedDropdown";
+import {VirtualizedDropdown} from "./VirtualizedDropdown";
 import PinCharts from "./PinCharts";
 import {useCoinListData} from "../../../Hooks/useCoinListData";
 
 const Pins = () => {
 
-    const [selectedCoin1, setSelectedCoin1] = useState('bitcoin')
-    const [selectedCoin2, setSelectedCoin2] = useState('ethereum')
+    const [selectedCoin1, setSelectedCoin1] = useState({label: 'bitcoin', value: 'bitcoin'})
+    const [selectedCoin2, setSelectedCoin2] = useState({label: 'ethereum', value: 'ethereum'})
 
     const {coinList, loading, error} = useCoinListData();
 
@@ -24,31 +24,31 @@ const Pins = () => {
         return <div>Error: {error}</div>;
     }
 
-    const handleDropDownSelect = (event, data, dropdown) => {
+    const handleDropDownSelect = (event, dropdown) => {
         if (dropdown === 1) {
-            setSelectedCoin1(data.value);
+            setSelectedCoin1(event);
         }
 
         if (dropdown === 2) {
-            setSelectedCoin2(data.value);
+            setSelectedCoin2(event);
         }
     }
 
     return (
         <div className="card pin-card">
-            <PinCharts className="chart1" coin={selectedCoin1}/>
+            <PinCharts className="chart1" coin={selectedCoin1.value}/>
             <div className="dropdown-area">
                 <VirtualizedDropdown
                     value={selectedCoin1}
                     options={coinList}
-                    onChange={(event, data) => handleDropDownSelect(event, data, 1)}/>
+                    handleOnChange={(event) => handleDropDownSelect(event, 1)}/>
                 <ArrowSvg className="arrow-svg"/>
                 <VirtualizedDropdown
                     value={selectedCoin2}
                     options={coinList}
-                    onChange={(event, data) => handleDropDownSelect(event, data, 1)}/>
+                    handleOnChange={(event) => handleDropDownSelect(event, 2)}/>
             </div>
-            <PinCharts className="chart2" coin={selectedCoin2}/>
+            <PinCharts className="chart2" coin={selectedCoin2.value}/>
         </div>
 
     )
