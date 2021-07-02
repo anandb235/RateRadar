3caa12d1f2c7e8fa0f8fc3296d8705f3336b6f68
Anand Borkar <borkaranand235@gmail.com>
2021-07-03 00:37:41 +0530

    Minor fixes

--- src/Components/MiniComponents/Home/ArrowSvg.js
+++ src/Components/MiniComponents/Home/ArrowSvg.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 function ArrowSvg({className}) {
 	return (
-		<svg className={className} style={{"margin-top": "-8px"}} viewBox="0 0 720 150" xmlns="http://www.w3.org/2000/svg">
+		<svg className={className} style={{"marginTop": "-8px"}} viewBox="0 0 720 150" xmlns="http://www.w3.org/2000/svg">
 			<g>
 				<line strokeWidth="15" y2="111" x2="653" y1="111" x1="81" stroke="#3b4ab8" fill="#3b4ab8" />
 				<line strokeWidth="15" y2="76" x2="653" y1="76" x1="81" stroke="#3b4ab8" fill="#3b4ab8" />
